84f16556eced312c63fd9f6906ddd1b19f882603
Julien Refour <julien@tensory.io>
2024-07-31 12:10:01 -0700

    remove fast journeys

--- src/odx/pipelines/odx/infer/nodes.py
+++ src/odx/pipelines/odx/infer/nodes.py
@@ -18,33 +18,15 @@ from functools import reduce
 from typing import List, Tuple, Union
 
 from loguru import logger
-from pyspark.sql import DataFrame as SparkDF, functions as F, Column
+from pyspark.sql import DataFrame as SparkDF, functions as F
 from pyspark.sql.types import StringType
 from pyspark.sql.window import Window
 
 from .impossible_conditions_and_descriptions import (
     get_impossible_conditions_and_descriptions,
 )
+from .utils import haversine_meters
 
-
-def haversine_meters(
-    lat_1_col: Column, lon_1_col: Column, lat_2_col: Column, lon_2_col: Column
-) -> Column:
-    haversine_column = (
-        2
-        * 6371000
-        * F.asin(  # haversine formula
-            F.sqrt(
-                F.sin((F.radians(lat_1_col) - F.radians(lat_2_col)) / 2) ** 2
-                + (
-                    F.cos(F.radians(lat_1_col))
-                    * F.cos(F.radians(lat_2_col))
-                    * F.sin((F.radians(lon_1_col) - F.radians(lon_2_col)) / 2) ** 2
-                )
-            )
-        )
-    )
-    return haversine_column
 
 
 def get_year_months(
